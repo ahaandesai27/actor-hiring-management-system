c6dc2141b5182c50204cd4f39afd6908f17a140d
ahaandesai27 <ahaan.desai2705@gmail.com>
2025-03-17 16:48:56 +0530

    Added CRUD for Professionals

--- Backend/Resources/controllers/professionalController.js
+++ Backend/Resources/controllers/professionalController.js
@@ -19,19 +19,53 @@ const ProfessionalController = {
     getAll: async (req, res) => {
         try {
             const Professionals = await Professional.findAll();
-            res.status(201).json(Professionals);
+            res.status(200).json(Professionals);
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
     },
     getOne: async (req, res) => {
-
+        try {
+            const {username} = req.params;
+            const professional = await Professional.findByPk(username);
+            res.status(200).json(professional);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     },
     update: async (req, res) => {
-
+        try {
+            const {username} = req.params;
+            const {info} = req.body;    
+            await Professional.update(
+                info,
+                {
+                    where: {
+                        username: username
+                    }
+                }
+            )
+            res.status(200).json("Updated successfully.");
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     },
     delete: async (req, res) => {
-
+        try {
+            const { username } = req.params;
+    
+            const deletedRows = await Professional.destroy({
+                where: { username: username }
+            });
+    
+            if (deletedRows === 0) {
+                return res.status(404).json({ message: "Professional not found." });
+            }
+    
+            res.status(200).json({ message: "Deleted successfully." });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
 }
 
--- Backend/Resources/routes/ProfessionalRoutes.js
+++ Backend/Resources/routes/ProfessionalRoutes.js
@@ -2,6 +2,9 @@ const ProfessionalController = require('../controllers/professionalController.js
 const express = require('express');
 const Router = express.Router();
 
+Router.get('/:username', ProfessionalController.getOne);
+Router.put('/:username', ProfessionalController.update);
+Router.delete('/:username', ProfessionalController.delete);
 Router.post('/', ProfessionalController.create);
 Router.get('/', ProfessionalController.getAll);
 
