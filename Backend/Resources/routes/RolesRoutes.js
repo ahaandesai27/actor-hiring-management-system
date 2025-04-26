93a41587556788f23e05d0bcf40467c076abacdc
AyaanshC <churiayaansh@gmail.com>
2025-04-26 22:48:12 +0530

    asfd

--- Backend/Resources/routes/RolesRoutes.js
+++ Backend/Resources/routes/RolesRoutes.js
@@ -3,7 +3,6 @@ const Router = express.Router();
 const FilmController = require('../controllers/filmController.js');
 const RoleController = require('../controllers/roleController.js');
 
-// Router.post('/', RoleController.create);
 Router.delete('/:role_id', RoleController.delete);
 
 module.exports = Router;
