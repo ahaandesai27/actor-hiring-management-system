1fc498f3f2acc79323bbc2bad88ebe50d636f406
ahaandesai27 <ahaan.desai2705@gmail.com>
2025-06-02 15:52:19 +0530

    Implemented basic chatting

---
+++ frontend/src/components/Chat/Messages/Incoming.jsx
@@ -0,0 +1,18 @@
+const Incoming = ({ message }) => {
+  return (
+    <div className="flex mb-4 cursor-pointer">
+      <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2" key={message.id}>
+        <img
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+          alt="User Avatar"
+          className="w-8 h-8 rounded-full"
+        />
+      </div>
+      <div className="flex max-w-96 bg-gold text-black rounded-lg p-3 gap-3">
+        <p className="text-gray-700">{message.message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Incoming;
