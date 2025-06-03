5068bb803ee96614b56ed4819bdbcb7c00d65b60
ahaandesai27 <ahaan.desai2705@gmail.com>
2025-06-03 15:19:56 +0530

    Added gitignore

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -6,13 +6,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "actor-hiring-system.firebaseapp.com",
   projectId: "actor-hiring-system",
   storageBucket: "actor-hiring-system.firebasestorage.app",
   messagingSenderId: "842393862290",
   appId: "1:842393862290:web:fe78da4d79ab665c93664b",
-  databaseURL: ""
+  databaseURL: import.meta.env.VITE_RTDB_URL
 };
 
 // Initialize Firebase
