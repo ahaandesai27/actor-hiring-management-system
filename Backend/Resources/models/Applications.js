87f91b7d10db27f2cf67d061f49bbe5d3d615cba
ahaandesai27 <ahaan.desai2705@gmail.com>
2025-06-05 17:32:23 +0530

    moved folder by accident

--- Backend/Resources/models/Applications.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize, DataTypes, DATE } = require('sequelize');
-const { db } = require('../../db/connectDB.js');
-
-const Applications = db.define('Applications', {
-    professional: DataTypes.STRING,
-    role_id: DataTypes.INTEGER,
-  }, { tableName: 'applications', timestamps: false });
-
-module.exports = Applications;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,126 @@
-import NewHeader from './components/NewHeader/NewHeader';
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
-import ProfilePage from './components/ProfilePage';
-import ViewRoleApplicants from './components/RolePage/ViewRoleApplicants.jsx';
-import RolePage from './components/RolePage/RolePage.jsx';
-import ProfessionalRolesPage from './components/RolePage/ProfessionalRolesPage.jsx';
-import useUser from './components/User/user.js';
-import { useEffect } from 'react';
+import {useUser} from './components/User/user.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+import LandingPage from './components/Landing/index.jsx';
+import HomePage from './components/Home/index.jsx';
+import Navbar from './components/Navbar.jsx';
+
+// auth
+import Login from './components/Auth/Login/Login.jsx';
+import SignIn from './components/Auth/SignIn/SignIn.jsx';
+
+//profile page 
+import ProfilePage from './components/Profile/ProfilePage';
+import EditProfile from './components/Profile/EditProfile/EditProfile.jsx';
+
+//roles 
+import ViewRoleApplicants from './components/Roles/RolePage/ViewRoleApplicants.jsx';
+import RolePage from './components/Roles/RolePage/RolePage.jsx';
+import ProfessionalRolesPage from './components/Roles/RolePage/ProfessionalRolesPage.jsx';
+import AddRole from './components/Roles/AddRole/AddRole.jsx';
+import ApplyRolePage from './components/Roles/RolePage/ApplyRolePage.jsx';
+
+//posts
+import Posts from './components/Posts/index.jsx';
+import Comments from './components/Posts/Comments.jsx';
+
+//chats 
+import ChatApp from './components/Chat/index.jsx';
+
 
 function App() {
-  const {updateUser} = useUser();
-
-  useEffect(() => updateUser('iamsrk', 'actor'));
+  const { userName } = useUser();
+  
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/roles/:role_id/applicants" element={<ViewRoleApplicants />} />
-      <Route path="/profile/:username/roles" element={<ProfessionalRolesPage />} />
-      <Route path="/profile/:username" element={<ProfilePage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path='/roles' element={<RolePage />} />
+      {/* Profile page */}
+      <Route path="/profile/edit" element={
+        <ProtectedRoute>
+          <><Navbar /> <EditProfile /></>
+        </ProtectedRoute>
+      } />
+      <Route path="/profile/:username/roles" element={
+        <ProtectedRoute>
+          <ProfessionalRolesPage />
+        </ProtectedRoute>
+      } />
+      <Route path="/profile/:username" element={
+        <ProtectedRoute>
+          <><Navbar /> <ProfilePage /></>
+        </ProtectedRoute>
+      } />
+      <Route path="/profile" element={
+        <ProtectedRoute>
+          <><Navbar /> <ProfilePage /></>
+        </ProtectedRoute>
+      } />
+
+      {/*Role page*/}
+      <Route path="/roles/apply/:roleId" element={
+        <ProtectedRoute>
+          <><Navbar /> <ApplyRolePage /></>
+        </ProtectedRoute>
+      } />
+      <Route path='/roles/add' element={
+        <ProtectedRoute>
+          <><Navbar /> <AddRole /></>
+        </ProtectedRoute>
+      } />
+      <Route path="/roles/:role_id/applicants" element={
+        <ProtectedRoute>
+          <ViewRoleApplicants />
+        </ProtectedRoute>
+      } />
+      <Route path='/roles' element={
+        <ProtectedRoute>
+          <><Navbar /> <RolePage /></>
+        </ProtectedRoute>
+      } />
+
+      {/*Posts page*/}
+      <Route path="/posts/:postId" element={
+        <ProtectedRoute>
+          <><Navbar /> <Comments /></>
+        </ProtectedRoute>
+      } />
+      <Route path="/posts" element={
+        <ProtectedRoute>
+          <><Navbar /> <Posts /></>
+        </ProtectedRoute>
+      } />
+
+      {/* Auth */}
+      <Route path='/login' element={
+          <Login />
+      } />
+      <Route path='/signin' element={
+          <SignIn />
+      } />
+
+      {/* Chats */}
+      <Route path='/chat' element={
+        <ProtectedRoute>
+          <><Navbar /> <ChatApp /></>
+        </ProtectedRoute>
+      } />
+
+
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <>
+              <Navbar />
+              <HomePage />
+            </>
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Public Landing Page */}
+      <Route path="/" element={<LandingPage />} />
     </Routes>
   );
 }
